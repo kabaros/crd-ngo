7274a3bdc8a44370d74506c9d44ad0d2e8b9d41e
mozafar <kabaros@gmail.com>
2020-05-02 12:41:44 +0100

    Update blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -4,30 +4,25 @@ import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
-  render() {
+  render () {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
+        <div className='full-width-image-container margin-top-0'>
           <h1
-            className="has-text-weight-bold is-size-1"
+            className='has-text-weight-bold is-size-1'
             style={{
               boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
               backgroundColor: '#f40',
               color: 'white',
-              padding: '1rem',
+              padding: '1rem'
             }}
           >
             Latest Stories
           </h1>
         </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
+        <section className='section'>
+          <div className='container'>
+            <div className='content'>
               <BlogRoll />
             </div>
           </div>
